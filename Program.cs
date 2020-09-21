b2b9f88b93c8842c176a8214303ebff0b4b6dd30
Ben Stoller <stollersystemsound@gmail.com>
2020-09-21 11:50:35 -0700

    ability to capture user input in two dictionaries
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,28 +1,31 @@
 using System;
 using System.Collections.Generic;
 
-namespace Queen {
+namespace QueenAttack{
     class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Enter coordinates for the QUEEN: ");
+      // INPUT FOR QUEEN'S COORDINATES
+      Console.WriteLine("Enter coordinates for the ATTACK QUEEN: ");
       string userInput= Console.ReadLine();
       String[] coords = userInput.Split(' ');
       // check if it was only 2 numbers! 
       int xCoord = int.Parse(coords[0]);
-      int xCoord = int.Parse(coords[1]);
+      int yCoord = int.Parse(coords[1]);
       Dictionary<string, int> attackQueenCoords = new Dictionary<string, int>() {{"x",xCoord}, {"y",yCoord}};
-      Console.WriteLine(attackQueenCoords);
-
-      // Console.WriteLine(coords[0],coords[1]);
-      // foreach (string x in coords)
-      // {
-      //   Console.WriteLine(x);
-      // }
-      
-      // Console.WriteLine("Enter coordinates for the QUEEN'S VICTIM!: ");
-      
+      Console.WriteLine(attackQueenCoords["x"].ToString());
+      Console.WriteLine(attackQueenCoords["y"].ToString());
+      // INPUT FOR QUEEN'S VICTIM
+      Console.WriteLine("Enter coordinates for the QUEEN's VICTIM: ");
+      string userInput2= Console.ReadLine();
+      String[] vCoords = userInput2.Split(' ');
+      // check if it was only 2 numbers! 
+      int xVCoord = int.Parse(vCoords[0]);
+      int yVCoord = int.Parse(vCoords[1]);
+      Dictionary<string, int> victimCoords = new Dictionary<string, int>() {{"x",xVCoord}, {"y",yVCoord}};
+      Console.WriteLine(victimCoords["x"].ToString());
+      Console.WriteLine(victimCoords["y"].ToString());      
     }
   }
 }
--- queen_attack.cs
+++ queen_attack.cs
@@ -1,76 +1,79 @@
 using System;
-using System.Collections.Generic;
 
-class Program
-{
-  public static void Main()
+namespace QueenAttack{
+  public class Game
   {
-    Console.WriteLine("Enter coordinates for the QUEEN: ");
-    string userInput= Console.ReadLine();
-    String[] coords = userInput.Split(' ');
-    // Console.WriteLine(coords[0],coords[1]);
-    foreach (string x in coords)
+    // public properties
+    public object VictimQueen { get; set; }
+    public object AttackQueen { get; set; }
+    public int BoardXLimit { get; set; }
+    public int BoardYLimit { get; set; }
+    public bool ConfirmedHit { get; set; }
+
+
+    // {x:1,y:1}
+    //constructor
+    public Game(object victimQueen, object attackQueen) 
     {
-      Console.WriteLine(x);
+      VictimQueen = victimQueen;
+      AttackQueen = attackQueen;
+      bool ConfirmedHit = false;
     }
-    Console.WriteLine("Enter coordinates for the QUEEN'S VICTIM!: ");
-    
+
+    // public method
+    public bool SearchAndDestroy()
+    {
+      CheckX();
+      CheckY();
+      CheckNE();
+      CheckSE();
+      CheckSW();
+      CheckNW();
+      return ConfirmedHit;
+    }
+
+    // private methods
+    private void CheckX()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
+    private void CheckY()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
+    private void CheckNE()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
+    private void CheckSE()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
+    private void CheckSW()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
+    private void CheckNW()
+    {
+      // if (VictimQueen.X == AttackQueen.Y)
+      // {
+      //   ConfirmedHit = true;
+      // }
+    }
   }
-}
-
-public class Queen
-{
-  // public properties
-  public string TargetQueen { get; set; }
-  public string AttackQueen { get; set; }
-  public string BoardXLimit { get; set; }
-  public string BoardYLimit { get; set; }
-
-
-  // {x:1,y:1}
-  //constructor
-  public Queen(string targetQueen, string attackQueen) 
-  {
-    TargetQueen = targetQueen;
-    AttackQueen = attackQueen;
-    // bool ConfirmedHit = false;
-  }
-
-  // public method
-  // public void SearchAndDestroy()
-  // {
-  //   CheckX();
-  //   CheckY();
-  //   CheckNE();
-  //   CheckSE();
-  //   CheckSW();
-  //   CheckNW();
-  // }
-
-  // private methods
-  // private void CheckX()
-  // {
-      
-  // }
-  
-  // private void CheckY()
-  // {
-    
-  // }
-  // private void CheckNE()
-  // {
-    
-  // }
-  // private void CheckSE()
-  // {
-    
-  // }
-  // private void CheckSW()
-  // {
-    
-  // }
-  // private void CheckNW()
-  // {
-    
-  // }
 }
